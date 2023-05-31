c19bf870c8c3b0f4af6e1060878622e0b1d141a5
reza.salari <rza.attacker@gmail.com>
2023-05-31 16:27:24 +0330

    update points

--- points.js
+++ points.js
@@ -1,8 +1,13 @@
 const Puppeteer = require("puppeteer");
 let page = "";
+
 async function doCrawl(){
 try {
-  const browser = await Puppeteer.launch({});
+  const browser = await Puppeteer.launch({ 
+   headless:true,
+    executablePath:'/usr/bin/chromium',
+   args:['--no-sandbox','--disable-setuid-sandbox']
+  });
   // Create a new page object
    page = await browser.newPage();
    console.log('open new Page');
@@ -12,10 +17,19 @@ try {
   });
 
   await page.waitForSelector("#technicalstudiesSubTabs");
+
   // await page.waitForSelector('button[id="onetrust-accept-btn-handler"]');
   // await page.click('button[id="onetrust-accept-btn-handler"]');
-  await page.click("xpath/" + '//*[@id="timePeriodsWidget"]/li[6]/a');
+  // await page.click("xpath/" + '//*[@id="timePeriodsWidget"]/li[6]/a');
   await page.waitForTimeout(8000)
+
+  await page.evaluate(() => {
+    let but = document.querySelector("#timePeriodsWidget > li.selected > a")
+    if (but) {
+        return Promise.resolve(but.click())
+    }
+})
+  // await page.waitForTimeout(60000)
   const riskyPoints = await getR2AndS2();
   await page.waitForTimeout(8000)
   await page.click("xpath/" + '//*[@id="timePeriodsWidget"]/li[7]/a');
@@ -32,32 +46,19 @@ try {
 }
 
 async function getR2AndS2() {
-  const pivotPointsTable = await page.$("#curr_table > tbody ");
-  const pivotPointsData = await page.evaluate((table) => {
-    const data = [];
-    const rows = table.querySelectorAll("tr");
-
-    rows.forEach((row) => {
-      const values = [];
-      const cells = row.querySelectorAll("td");
-
-      cells.forEach((cell) => {
-        values.push(cell.innerText);
-      });
-
-      if (values.length) {
-        data.push(values);
-      }
+  const result = await page.$$eval('#curr_table', rows => {
+    return Array.from(rows, row => {
+      const columns = row.querySelectorAll('td');
+      return Array.from(columns, column => column.innerText);
     });
+  });
+console.log(result);
 
-    return data;
-  }, pivotPointsTable);
-
-  const S2 = pivotPointsData[0][2];
-  const R2 = pivotPointsData[0][6];
-  return {
-    Sell: Number(R2.split('.')[0]) - 1 +'.'+R2.split('.')[1] ,
-   Buy: Number(S2.split('.')[0]) + 1 +'.'+S2.split('.')[1] } 
+  // const S2 = pivotPointsData[0][2];
+  // const R2 = pivotPointsData[0][6];
+  // return {
+  //   Sell: Number(R2.split('.')[0]) - 1 +'.'+R2.split('.')[1] ,
+  //  Buy: Number(S2.split('.')[0]) + 1 +'.'+S2.split('.')[1] } 
 }
 doCrawl()
 module.exports = doCrawl
